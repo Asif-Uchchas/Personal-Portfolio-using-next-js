779f19b25a2ab8817624b0ecc5d2570eca158836
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2023-12-13 14:53:50 +0600

    added StarBackground to the project for better visualization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import StarsCanvas from '@/components/main/StarBackground'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
-        {children}</body>
+        <StarsCanvas/>
+        {children}
+      </body>
     </html>
   )
 }
