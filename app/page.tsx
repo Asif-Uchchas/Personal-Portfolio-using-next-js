6bf0ede1c913e2e1adc1900ba5daf268714f3a11
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2023-12-16 04:25:26 +0600

    Added Projects and Footer section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Encryption from '@/components/main/Encryption'
 import { Hero } from '@/components/main/Hero'
+import Projects from '@/components/main/Projects'
 import Skills from '@/components/main/Skills'
 import Image from 'next/image'
 
@@ -9,7 +10,8 @@ export default function Home() {
       <div className='flex flex-col gap-20'>
         <Hero />
         <Skills />
-        <Encryption/>
+        <Encryption />
+        <Projects/>
       </div>
     </main>
   )
